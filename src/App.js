ad06af62351becea22f7bec9964350e60cce7003
entimaa <entimaa9122003@gmail.com>
2024-01-09 13:53:31 +0200

    ADD DOCKER

--- src/App.js
+++ src/App.js
@@ -1,37 +1,26 @@
 import React,{useEffect,useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
 import axios from 'axios';
 
 function App() {
 
-    const [message,setMessage] =useState("")
-  const loadDataFromServer = () => {
-    axios.get('http://localhost:5001/api/sendData')
+    const [users,setUsers] =useState([])
+    useEffect(()=>{
+      loadData()
+    },[])
+
+  const loadData = () => {
+    axios.get('http://localhost:5004/api/users')
     .then(result => {
-setMessage(result.data.message)
+setUsers(result.data.users)
     })
     .catch(error => console.log(error.message))
   }
-  useEffect(()=>{
-    loadDataFromServer();
-  },[])
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+     users.map(user => <li className='list-group-item'><b>{user.name}</b> | {user.email}</li>)
+      }
     </div>
   );
 }
