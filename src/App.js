84d103298fdb8984c7fbe37c31fa9e259b74f40f
entimaa <entimaa9122003@gmail.com>
2024-01-09 18:09:04 +0200

    change name the file

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React,{useEffect,useState} from 'react';
+import axios from 'axios';
+
+function App() {
+
+    const [users,setUsers] =useState([])
+    useEffect(()=>{
+      loadData()
+    },[])
+
+  const loadData = () => {
+    axios.get('http://34.207.76.158:5004/api/users')
+    .then(result => {
+setUsers(result.data.users)
+    })
+    .catch(error => console.log(error.message))
+  }
+ 
+  return (
+    <div className="App">
+      {
+     users.map(user => <li className='list-group-item'><b>{user.name}</b> | {user.email}</li>)
+      }
+    </div>
+  );
+}
+
+export default App;
